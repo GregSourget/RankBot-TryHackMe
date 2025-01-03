3a66470e281eee6641e230a5f60da3e5d07bdb01
GregSourget <sourgetgregoire@mail.com>
2025-01-03 16:13:42 +0100

    update with json credentials

--- main.py
+++ main.py
@@ -1,14 +1,33 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+import time
+import json
 
 browser = webdriver.Firefox()
 browser.get('https://tryhackme.com/r/login')
 
-browser.implicitly_wait(10)
+def read_credentials(filename):
+    with open(filename, 'r') as file:
+        credentials = json.load(file)
+    return credentials
 
-login_field = browser.find_element(By.ID, 'username-or-email-field')
-login_field.clear()
-login_field.send_keys('test') 
+def login(browser, user, mdp):
+    login_field = browser.find_element(By.ID, 'username-or-email-field')
+    login_field.clear()
+    login_field.send_keys(user)
 
-# browser.quit()
+    password_field = browser.find_element(By.ID, 'password-field')
+    password_field.clear()
+    password_field.send_keys(mdp)
+
+    password_field.send_keys(Keys.RETURN)
+
+credentials = read_credentials('credentials.json')
+print(credentials)
+
+user = credentials['login']
+mdp = credentials['password']
+
+login(browser, user, mdp)
+
