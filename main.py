719a528d17a60138d37c993daf047deeb9f519fc
GregSourget <sourgetgregoire@mail.com>
2025-01-03 20:46:04 +0100

    captcha bypass

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-import time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import json
+import time
 
 browser = webdriver.Firefox()
 browser.get('https://tryhackme.com/r/login')
@@ -12,7 +14,21 @@ def read_credentials(filename):
         credentials = json.load(file)
     return credentials
 
+def bypass_captcha(browser):
+    """Bypass reCAPTCHA"""
+    try:
+        WebDriverWait(browser, 10).until(EC.frame_to_be_available_and_switch_to_it(
+            (By.CSS_SELECTOR, "iframe[src^='https://www.google.com/recaptcha/api2/anchor']")))
+        
+        WebDriverWait(browser, 10).until(EC.element_to_be_clickable(
+            (By.XPATH, "//span[@id='recaptcha-anchor']"))).click()
+
+        time.sleep(5)
+    except Exception as exception:
+        print("Error during CAPTCHA bypass:", exception)
+
 def login(browser, user, mdp):
+    """Whole login process"""
     login_field = browser.find_element(By.ID, 'username-or-email-field')
     login_field.clear()
     login_field.send_keys(user)
@@ -25,11 +41,10 @@ def login(browser, user, mdp):
 
 credentials = read_credentials('credentials.json')
 print(credentials)
-print(credentials)
-
 
 user = credentials['login']
 mdp = credentials['password']
 
-login(browser, user, mdp)
+bypass_captcha(browser)
 
+login(browser, user, mdp)
